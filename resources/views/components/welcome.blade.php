a8ffb1365ebfe747e9da229e9b1ba6cf3e1722ad
tharaka911 <tharakahitibandara@gmail.com>
2023-06-01 00:08:48 +0530

    the old log removed and regular user dashboad is completed

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,7 +2,7 @@
     <table class="min-w-full divide-y divide-gray-700 dark:divide-gray-600">
         <thead>
             <tr>
-                <th class="py-3 px-4 text-left text-gray-300 dark:text-gray-400">ID</th>
+                <th class="py-3 px-4 text-left text-gray-300 dark:text-gray-400">No</th>
                 <th class="py-3 px-4 text-left text-gray-300 dark:text-gray-400">Project Name</th>
                 <th class="py-3 px-4 text-left text-gray-300 dark:text-gray-400">Domain Name</th>
                 <th class="py-3 px-4 text-left text-gray-300 dark:text-gray-400">Date</th>
@@ -14,10 +14,10 @@
             <!-- Example project rows -->
             <tr class="bg-gray-700 dark:bg-gray-800">
                 <td class="py-4 px-6 text-gray-300 dark:text-gray-400">1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example.com</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 30, 2023</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 1 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 6</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example6.com</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 31, 2023</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 6 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
                 <td class="py-4 px-6">
                     <button class="bg-red-500 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded">Delete</button>
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Logs</button>
@@ -25,11 +25,11 @@
             </tr>
 
             <tr class="bg-gray-700 dark:bg-gray-800">
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example.com</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 30, 2023</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 1 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">2</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 5</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example5.com</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 31, 2023</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 5 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
                 <td class="py-4 px-6">
                     <button class="bg-red-500 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded">Delete</button>
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Logs</button>
@@ -37,11 +37,11 @@
             </tr>
 
             <tr class="bg-gray-700 dark:bg-gray-800">
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example.com</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">3</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 4</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example4.com</td>
                 <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 30, 2023</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 1 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 4 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
                 <td class="py-4 px-6">
                     <button class="bg-red-500 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded">Delete</button>
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Logs</button>
@@ -49,11 +49,11 @@
             </tr>
 
             <tr class="bg-gray-700 dark:bg-gray-800">
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example.com</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 30, 2023</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 1 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">4</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 3</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example3.com</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 29, 2023</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 3 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
                 <td class="py-4 px-6">
                     <button class="bg-red-500 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded">Delete</button>
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Logs</button>
@@ -61,11 +61,11 @@
             </tr>
 
             <tr class="bg-gray-700 dark:bg-gray-800">
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example.com</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 30, 2023</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 1 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">5</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 2</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example2.com</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 28, 2023</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 2 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
                 <td class="py-4 px-6">
                     <button class="bg-red-500 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded">Delete</button>
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Logs</button>
@@ -73,10 +73,10 @@
             </tr>
 
             <tr class="bg-gray-700 dark:bg-gray-800">
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">1</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">6</td>
                 <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example.com</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 30, 2023</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example1.com</td>
+                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 25, 2023</td>
                 <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 1 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
                 <td class="py-4 px-6">
                     <button class="bg-red-500 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded">Delete</button>
@@ -84,15 +84,5 @@
                 </td>
             </tr>
 
-            <tr class="bg-gray-700 dark:bg-gray-800">
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">Project 1</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">example.com</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">May 30, 2023</td>
-                <td class="py-4 px-6 text-gray-300 dark:text-gray-400">API Key 1 <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Copy</button></td>
-                <td class="py-4 px-6">
-                    <button class="bg-red-500 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded">Delete</button>
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Logs</button>
-                </td>
-            </tr>
+            
         <tbody>
